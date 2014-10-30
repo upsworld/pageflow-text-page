e870f5be0b7304bc665151f01e6536ba3c418875
Christoph Merkelbach <cmerkelbach@codevise.de>
2014-10-30 16:26:05 +0100

    initial front end commit / needs cleanup

--- app/assets/javascript/pageflow/text_page/page_type.js
+++ app/assets/javascript/pageflow/text_page/page_type.js
@@ -2,9 +2,46 @@ pageflow.pageType.register('text_page', _.extend({
 
   prepareNextPageTimeout: 0,
 
-  enhance: function(pageElement, configuration) {},
+  enhance: function(pageElement, configuration) {
+    this.pageSpacerElement = pageElement.find('.page_spacer');
+    this.contentArea = pageElement.find('.contentText');
+    this.backgroundArea = pageElement.find('.background');
+    this.titleArea = pageElement.find('.contentInnerWrapper .page_header');
+    this.resizePageSpacer(pageElement, configuration);
+    /* for min height: pageElement.find('.contentInnerWrapper').css('min-height', pageElement.height() + 'px'); */
 
-  resize: function(pageElement, configuration) {},
+    this.iscroll = pageElement.find('.content.scroller').data('pageflow-scroller').iscroll;
+    $(pageElement).on('scrollermove', this.applyBackgroundEffects.bind(this, pageElement));
+  },
+
+  applyBackgroundEffects: function(pageElement) {
+    pageElement.find('.backgroundArea.dim .background, .backgroundArea.dim .fixedHeaderArea').css('opacity', (0.5 * pageElement.height() + this.iscroll.y)/(pageElement.height()*0.5)); // Abblenden */
+    pageElement.find('.backgroundArea.parallax .background').css('-webkit-transform', "translateY(" + this.iscroll.y *0.05 + "px)"); // Parallax
+  },
+
+  resizePageSpacer: function(pageElement, configuration) {
+    /* for fullscreen: this.pageSpacerElement.css('height', pageElement.height() + 'px'); */
+    if(pageElement.hasClass('banner_only')) {
+      this.pageSpacerElement.css('height', pageElement.height() / 3 + this.titleArea.height() + 'px');
+    }
+    else if (pageElement.hasClass('title_only')) {
+      this.pageSpacerElement.css('height', pageElement.height() + 'px');
+    }
+    else {
+      if(pageElement.height() - 200 > 200) {
+        this.pageSpacerElement.css('height', pageElement.height() - 200 + 'px');
+      }
+    }
+
+    this.backgroundArea.css('height', this.pageSpacerElement.height() + 'px');
+
+    this.contentArea.css('min-height', pageElement.height() + 'px'); //min-heights for white area/ text area for short text?
+  },
+
+  resize: function(pageElement, configuration) {
+    this.resizePageSpacer(pageElement, configuration);
+    this.applyBackgroundEffects(pageElement);
+  },
 
   prepare: function(pageElement, configuration) {},
 
@@ -12,9 +49,12 @@ pageflow.pageType.register('text_page', _.extend({
     return pageflow.preload.backgroundImage(pageElement.find('.background_image'));
   },
 
-  activating: function(pageElement, configuration) {},
+  activating: function(pageElement, configuration) {
+    this.applyBackgroundEffects(pageElement);
+  },
 
-  activated: function(pageElement, configuration) {},
+  activated: function(pageElement, configuration) {
+  },
 
   deactivating: function(pageElement, configuration) {},
 
