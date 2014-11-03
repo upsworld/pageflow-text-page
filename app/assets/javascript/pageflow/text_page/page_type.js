9ad2c5063dff06abd063d7ba686909faf7f61443
Tim Fischbach <tfischbach@codevise.de>
2014-11-03 16:26:03 +0100

    Page Type Changes
    * use scroller widget positionY instead of using internal iscroll.y
    * control scroll indicator invert
    * use less classes on page element

--- app/assets/javascript/pageflow/text_page/page_type.js
+++ app/assets/javascript/pageflow/text_page/page_type.js
@@ -4,33 +4,34 @@ pageflow.pageType.register('text_page', _.extend({
 
   enhance: function(pageElement, configuration) {
     pageElement.addClass('text-page');
-    this.iscroll = pageElement.find('.content.scroller').data('pageflow-scroller').iscroll;
+    this.content = pageElement.find('.scroller');
     this.pageSpacerElement = pageElement.find('.page_spacer');
     this.contentArea = pageElement.find('.contentText');
     this.backgroundArea = pageElement.find('.background');
     this.titleArea = pageElement.find('.contentInnerWrapper .page_header');
-    this.resizePageSpacer(pageElement, configuration);
-    /* for min height: pageElement.find('.contentInnerWrapper').css('min-height', pageElement.height() + 'px'); */
 
-    $(pageElement).on('scrollermove', this.applyBackgroundEffects.bind(this, pageElement));
+    this.resizePageSpacer(pageElement, configuration);
+
+    pageElement.data('invertIndicator', !configuration.invert_text);
+    pageElement.on('scrollermove', this.applyBackgroundEffects.bind(this, pageElement, configuration));
   },
 
-  applyBackgroundEffects: function(pageElement) {
+  applyBackgroundEffects: function(pageElement, configuration) {
     var spacerPageRatio = this.pageSpacerElement.height() / pageElement.height();
+    var y = this.content.scroller('positionY');
 
-    pageElement.find('.backgroundArea .fixedHeaderArea').css('opacity', (0.5 * pageElement.height() + this.iscroll.y)/(pageElement.height() * 0.5)); // Abblenden des Titels, immer*/
+    pageElement.find('.backgroundArea .fixedHeaderArea').css('opacity', (0.5 * pageElement.height() + y)/(pageElement.height() * 0.5)); // Abblenden des Titels, immer*/
 
-    console.log('pageSpacerElement: ' + this.pageSpacerElement.height() + " pageElement: " + pageElement.height());
-    if(pageElement.hasClass('topasset-dim')) {
-      pageElement.find('.backgroundArea .background').css('opacity', (spacerPageRatio * pageElement.height() + this.iscroll.y)/(pageElement.height() * spacerPageRatio)); // Abblenden */
+    if(configuration.topasset_dim) {
+      pageElement.find('.backgroundArea .background').css('opacity', (spacerPageRatio * pageElement.height() + y)/(pageElement.height() * spacerPageRatio)); // Abblenden */
     }
     else {
       pageElement.find('.backgroundArea .background').css('opacity', 1);
     }
 
-    var parallaxPosition = this.iscroll.y *0.05;
+    var parallaxPosition = y *0.05;
 
-    if(pageElement.hasClass('topasset-parallax')) {
+    if(configuration.topasset_parallax) {
       pageElement.find('.backgroundArea.parallax .background').css({"-webkit-transform":"translateY(" + parallaxPosition + "px)", "-moz-transform":"translateY(" + parallaxPosition + "px)", "-ms-transform":"translateY(" + parallaxPosition + "px)", "-o-transform":"translateY(" + parallaxPosition + "px)", "transform":"translateY(" + parallaxPosition + "px)"}); // Parallax
     }
     else {
@@ -39,16 +40,14 @@ pageflow.pageType.register('text_page', _.extend({
   },
 
   resizePageSpacer: function(pageElement, configuration) {
-
-    /* for fullscreen: this.pageSpacerElement.css('height', pageElement.height() + 'px'); */
-    if(pageElement.hasClass('banner')) {
+    if (pageElement.hasClass('banner')) {
       this.pageSpacerElement.css('height', pageElement.height() / 3 + this.titleArea.height() + 'px');
     }
     else if (pageElement.hasClass('title_only')) {
       this.pageSpacerElement.css('height', pageElement.height() + 'px');
     }
     else {
-      if(pageElement.height() - 200 > 200) {
+      if (pageElement.height() - 200 > 200) {
         this.pageSpacerElement.css('height', pageElement.height() - 200 + 'px');
       }
     }
@@ -56,12 +55,12 @@ pageflow.pageType.register('text_page', _.extend({
     this.backgroundArea.css('height', this.pageSpacerElement.height() + 'px');
 
     this.contentArea.css('min-height', pageElement.height() + 'px'); //min-heights for white area/ text area for short text?
-    this.iscroll.refresh();
+    this.content.scroller('refresh');
   },
 
   resize: function(pageElement, configuration) {
     this.resizePageSpacer(pageElement, configuration);
-    this.applyBackgroundEffects(pageElement);
+    this.applyBackgroundEffects(pageElement, configuration);
   },
 
   prepare: function(pageElement, configuration) {
@@ -73,7 +72,7 @@ pageflow.pageType.register('text_page', _.extend({
 
   activating: function(pageElement, configuration) {
     this.resizePageSpacer(pageElement, configuration);
-    this.applyBackgroundEffects(pageElement);
+    this.applyBackgroundEffects(pageElement, configuration);
   },
 
   activated: function(pageElement, configuration) {
@@ -104,17 +103,15 @@ pageflow.pageType.register('text_page', _.extend({
       pageElement.toggleClass(option, configuration.get('text_coverage') === option);
     });
 
-    pageElement.toggleClass('invert-textarea', configuration.get('invert_text'));
-
-    pageElement.toggleClass('topasset-dim', configuration.get('topasset_dim'));
-    pageElement.toggleClass('topasset-parallax', configuration.get('topasset_parallax'));
+    pageElement.toggleClass('invert-textarea', !!configuration.get('invert_text'));
+    pageElement.data('invertIndicator', !configuration.get('invert_text'));
 
     pageElement.find('.shadow, .header-background-layer').css({
       opacity: configuration.get('gradient_opacity') / 100
     });
 
     this.resizePageSpacer(pageElement, configuration);
-    this.applyBackgroundEffects(pageElement);
+    this.applyBackgroundEffects(pageElement, configuration.attributes);
   },
 
   embeddedEditorViews: function() {
