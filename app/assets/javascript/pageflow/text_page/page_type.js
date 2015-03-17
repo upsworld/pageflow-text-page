9949a1b3b3047be1651f9cf7959685149b5d52b0
Christoph Merkelbach <cmerkelbach@codevise.de>
2015-03-17 20:27:11 +0100

    fix the fix

--- app/assets/javascript/pageflow/text_page/page_type.js
+++ app/assets/javascript/pageflow/text_page/page_type.js
@@ -66,7 +66,7 @@ pageflow.pageType.register('text_page', _.extend({
 
     pageElement.find('.backgroundArea .fixed_header_area').css('opacity', (0.6 * dimHeightTitle + y)/(dimHeightTitle * 0.6)); // Abblenden des Titels, immer*/
 
-    if(!!configuration.topasset_dim) {
+    if(configuration.topasset_dim) {
       pageElement.find('.backgroundArea .background').css('opacity', 1 - (-y / dimHeight)); // Abblenden */
     }
     else {
@@ -75,7 +75,7 @@ pageflow.pageType.register('text_page', _.extend({
 
     var parallaxPosition = y * 0.2;
 
-    if(!!configuration.topasset_parallax) {
+    if(configuration.topasset_parallax) {
       pageElement.find('.backgroundArea .background').css({"-webkit-transform":"translateY(" + parallaxPosition + "px)", "-moz-transform":"translateY(" + parallaxPosition + "px)", "-ms-transform":"translateY(" + parallaxPosition + "px)", "-o-transform":"translateY(" + parallaxPosition + "px)", "transform":"translateY(" + parallaxPosition + "px)"}); // Parallax
     }
     else {
@@ -188,7 +188,7 @@ pageflow.pageType.register('text_page', _.extend({
 
     pageElement.find('.content_and_background').toggleClass('no_background_image', !configuration.getImageFile('background_image_id'));
 
-    pageElement.find('.content_and_background').toggleClass('sticky_inline_image', configuration.get('sticky_inline_image'));
+    pageElement.find('.content_and_background').toggleClass('sticky_inline_image', !!configuration.get('sticky_inline_image'));
 
     if(configuration.get('text_position') == "left" || configuration.get('text_position') == "right") {
       this.titleArea = pageElement.find('.backgroundArea .fixed_header_area');
